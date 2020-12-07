ff2898558e4ba27860bdc102a1a2dfe9a56ede4e
Graham Squires <graham.squires3@gmail.com>
2020-12-07 18:58:40 -0500

    backend working following in class dog app example

--- models.py
+++ models.py
@@ -3,17 +3,16 @@ import datetime
 # Connect to a Postgres database.
 DATABASE = PostgresqlDatabase('flask_muse_app', host='localhost', port=5432)
 
-class Song(Model)
+class Song(Model):
     title = CharField()
     artist = CharField()
     album = CharField()
     created_at = DateTimeField(default=datetime.datetime.now)
-    
     class Meta:
         database = DATABASE
         
-        def initialize():
-        DATABASE.connect()
+def initialize():
+    DATABASE.connect()
     DATABASE.create_tables([Song], safe=True)
     print("TABLES Created")
     DATABASE.close()
